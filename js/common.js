96c300f5c82a6bd218b23b28c5983e9fddc3d39b
Valera19900 <kaktuz34@gmail.com>
2014-09-19 20:47:21 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -6,11 +6,11 @@
 				var scrollTop = $(window).scrollTop();
 				var navHeight = 143;
 				if (scrollTop > 332 - navHeight) scrollTop = 332 - navHeight;
-					$('#js-header').css('height', 332-scrollTop+'px');
-					$('#js-header-text').css('font-size', (2 - (2 / 360) * scrollTop)+'em');
-					$('#js-header-text').css('margin-top', (33 - (33 / 260) * scrollTop)+'px');
-					$('#js-logo').css('margin-top', (40 - (33 / 240) * scrollTop)+'px');
-					$('#js-logo').css('width', (233 - (233 / 466) * scrollTop)+'px');
+					$('.js-header').css('height', 332-scrollTop+'px');
+					$('.js-header-text').css('font-size', (2 - (2 / 360) * scrollTop)+'em');
+					$('.js-header-text').css('margin-top', (33 - (33 / 260) * scrollTop)+'px');
+					$('.js-logo').css('margin-top', (40 - (33 / 240) * scrollTop)+'px');
+					$('.js-logo').css('width', (233 - (233 / 466) * scrollTop)+'px');
 				 
 			}
 			calcHeader();
