6ce833ebfdfea44167dcdbdf0b4080f5e2a27711
Valera19900 <kaktuz34@gmail.com>
2014-09-19 20:39:42 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,8 +1,24 @@
-head.ready(function() {
+(function ($, window, document, undefined) {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$(document).ready(function() {
+		if($('body').hasClass('main')) {
+			var calcHeader = function() {
+				var scrollTop = $(window).scrollTop();
+				var navHeight = 143;
+				if (scrollTop > 332 - navHeight) scrollTop = 332 - navHeight;
+					$('#js-header').css('height', 332-scrollTop+'px');
+					$('#js-header-text').css('font-size', (2 - (2 / 360) * scrollTop)+'em');
+					$('#js-header-text').css('margin-top', (33 - (33 / 260) * scrollTop)+'px');
+					$('#js-logo').css('margin-top', (40 - (33 / 240) * scrollTop)+'px');
+					$('#js-logo').css('width', (233 - (233 / 466) * scrollTop)+'px');
+				 
+			}
+			calcHeader();
+			$(window).bind('scroll', function() {
+				calcHeader();
+			});
+		}
+	});
 
-	// console.log($('body').html());
-});
+})(jQuery, this, this.document);
+ 
